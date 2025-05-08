76db2125d3cdb4d448eb85059f4371aefd936611
bejgli <76849448+bejgli@users.noreply.github.com>
2025-05-08 14:10:25 +0200

    Add new outputs to db

--- autonomous_db/outputs.tf
+++ autonomous_db/outputs.tf
@@ -1,3 +1,15 @@
 output "db_conn" {
   value = oci_database_autonomous_database.terraform_adb.connection_strings
 }
+
+output "high" {
+  value = oci_database_autonomous_database.terraform_adb.connection_strings.high
+}
+
+output "profiles" {
+  value = oci_database_autonomous_database.terraform_adb.connection_strings.profiles
+}
+
+output "values" {
+  value = oci_database_autonomous_database.terraform_adb.connection_strings.profiles.value
+}
